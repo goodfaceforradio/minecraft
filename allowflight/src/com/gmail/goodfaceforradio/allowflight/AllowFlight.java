98d73b39fe91855a6caffc605d0031af2c05b707
Ola <goodfaceforradio@gmail.com>
2012-11-23 23:34:04 +0100

    Removed debug log printouts.

--- allowflight/src/com/gmail/goodfaceforradio/allowflight/AllowFlight.java
+++ allowflight/src/com/gmail/goodfaceforradio/allowflight/AllowFlight.java
@@ -10,15 +10,13 @@ public class AllowFlight extends JavaPlugin implements Listener {
 
 	// Executed when the plugin is enabled (automatically at server startup).
 	public void onEnable() {
-	    // Register for events to receive calls to onProjectileHit.
-        getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("onEnable has been invoked!");        
+            // Register for events to receive calls to onProjectileHit.
+            getServer().getPluginManager().registerEvents(this, this);
 	}		
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent evt) {
-	    Player player = evt.getPlayer();
-        getLogger().info("player joined, setting allow flight.");	    
-        player.setAllowFlight(true);
+            Player player = evt.getPlayer();
+            player.setAllowFlight(true);
 	}
 }
